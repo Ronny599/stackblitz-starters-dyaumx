4f103d2fdc4e840fd032e7e8e461f19bbe7747c8
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-17 18:57:09 +0530

    1 file modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -11,7 +11,6 @@ export default function Navigate() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dashboard
