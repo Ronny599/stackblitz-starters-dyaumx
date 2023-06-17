225a4282538833dc4a1cc8773a940dc8343d2383
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-17 18:51:20 +0530

    1 file modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -11,12 +11,7 @@ export default function Navigate() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
+              
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dashboard
@@ -27,9 +22,6 @@ export default function Navigate() {
                   <li><hr className="dropdown-divider"/></li>
                   <li><a className="dropdown-item" href="#">Something else here</a></li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
             
