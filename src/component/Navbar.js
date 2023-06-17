2f4e54793d44465f672d1f42f498fea90466b173
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-17 18:47:50 +0530

    1 file modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -19,7 +19,7 @@ export default function Navigate() {
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+                  Dashboard
                 </a>
                 <ul className="dropdown-menu">
                   <li><a className="dropdown-item" href="#">Action</a></li>
@@ -32,10 +32,7 @@ export default function Navigate() {
                 <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            
           </div>
         </div>
       </nav>
