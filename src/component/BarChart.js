e7857ccf7dffabde0f7c4f2c281a9a7a06b5002f
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-21 12:16:52 +0530

    Update BarChart.js

--- src/component/BarChart.js
+++ src/component/BarChart.js
@@ -1,39 +1,68 @@
  import React from 'react';
 
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
+const design ={
+  width:'500',
+  height:'500'
+}
 
-function BarChart () {
+export default function BarChart () {
+
+// Set Dimensions
+const xSize = 500; 
+const ySize = 500;
+const margin = 40;
+const xMax = xSize - margin*2;
+const yMax = ySize - margin*2;
+
+// Create Random Points
+const numPoints = 100;
+const data = [];
+for (let i = 0; i < numPoints; i++) {
+  data.push([Math.random() * xMax, Math.random() * yMax]);
+}
+
+// Append SVG Object to the Page
+const svg = d3.select("#myPlot")
+  .append("svg")
+  .append("g")
+  .attr("transform","translate(" + margin + "," + margin + ")");
+
+// X Axis
+const x = d3.scaleLinear()
+  .domain([0, 500])
+  .range([0, xMax]);
+
+svg.append("g")
+  .attr("transform", "translate(0," + yMax + ")")
+  .call(d3.axisBottom(x));
+
+// Y Axis
+const y = d3.scaleLinear()
+  .domain([0, 500])
+  .range([ yMax, 0]);
+
+svg.append("g")
+  .call(d3.axisLeft(y));
+
+// Dots
+svg.append('g')
+  .selectAll("dot")
+  .data(data).enter()
+  .append("circle")
+  .attr("cx", function (d) { return d[0] } )
+  .attr("cy", function (d) { return d[1] } )
+  .attr("r", 3)
+  .style("fill", "Red");
+
 
   return (
 
-    <svg
-
-      style={{
-
-        height: 500,
-
-        width: "100%",
-
-        marginRight: "0px",
-
-        marginLeft: "0px",
-
-      }}
-
-    >
-
-      <g className="plot-area" />
-
-      <g className="x-axis" />
-
-      <g className="y-axis" />
-
-    </svg>
-
+    <svg className="bar" id="myPlot" style={design}></svg>
   );
 
 }
 
 
-export default BarChart;
+// export default BarChart;
