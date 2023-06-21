5364f7b8b06ab3dc54d875ad3ec01c988e407623
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-21 12:17:16 +0530

    Update Menu.js

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
+
 export default function Menu() {
   return (
     <>
+    <div className="container">
       <form>
-        <div className="mb-3">
+        <div className="m-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
           </label>
@@ -13,12 +15,13 @@ export default function Menu() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            cols=""
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
           </div>
         </div>
-        <div className="mb-3">
+        <div className="m-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
             Password
           </label>
@@ -28,20 +31,26 @@ export default function Menu() {
             id="exampleInputPassword1"
           />
         </div>
-        <div className="mb-3 form-check">
+        <div class="m-3">
+  <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" cols="2"></textarea>
+</div>
+        <div className="mb-3 ml-3 form-check">
           <input
             type="checkbox"
             className="form-check-input"
             id="exampleCheck1"
           />
           <label className="form-check-label" htmlFor="exampleCheck1">
-            Please check all the information as if information filled is incorrect you are going to be responsible for it.
+            Please check all the information as if information filled is
+            incorrect you are going to be responsible for it.
           </label>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
       </form>
+    </div>
     </>
   );
 }
