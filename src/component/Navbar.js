d4208e1562cbcaa244d55cb24a46b1a507455b96
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-06-25 15:20:01 +0530

    1 file modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,36 +1,36 @@
 import * as React from 'react';
 
-export default function Navigate() {
-  const [myStyle, setMyStyle]= React.useState({
-    // let myStyle={
-    color: "black",
-    backgroundColor: "white"
+export default function Navigate(props) {
+  // const [myStyle, setMyStyle]= React.useState({
+  //   // let myStyle={
+  //   color: "black",
+  //   backgroundColor: "white"
+  // // }
+  // })
+
+  // const [btnText, setBtnText]= React.useState("Enable Dark Mode")
+
+  // const toggleMode= ()=>{
+  //   if(myStyle.color==="black"){
+  //     setMyStyle({
+  //       color: "white",
+  //       backgroundColor: "black"
+  //     })
+  //     setBtnText("Enable Light Mode")
+  //   }
+  //   else{
+  //     setMyStyle({
+  //       color: "black",
+  //       backgroundColor: "white"
+  //     })
+  //     setBtnText("Enable Dark Mode")
+  //   }
   // }
-  })
-
-  const [btnText, setBtnText]= React.useState("Enable Dark Mode")
-
-  const toggleMode= ()=>{
-    if(myStyle.color==="black"){
-      setMyStyle({
-        color: "white",
-        backgroundColor: "black"
-      })
-      setBtnText("Enable Light Mode")
-    }
-    else{
-      setMyStyle({
-        color: "black",
-        backgroundColor: "white"
-      })
-      setBtnText("Enable Dark Mode")
-    }
-  }
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">        
-        <div className="container-fluid" style={myStyle}>
-          <a className="navbar-brand" href="#" style={myStyle}>
+      <nav className='navbar navbar-expand-lg navbar-${props.type}bg-body-tertiary'>        
+        <div className="container-fluid" /*style={myStyle}*/>
+          <a className="navbar-brand" href="#" /*style={myStyle}*/>
             Dashboard
           </a>
           <button
@@ -41,11 +41,11 @@ export default function Navigate() {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
-            style={myStyle}
+            // style={myStyle}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent" style={myStyle}>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent" /*style={myStyle}*/>
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item dropdown">
                 <a
@@ -54,44 +54,44 @@ export default function Navigate() {
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
-                  style={myStyle}
+                  // style={myStyle}
                 >
                   Dashboard
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item" href="#" style={myStyle}>
+                    <a className="dropdown-item" href="#" /*style={myStyle}*/>
                       Chart
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#" style={myStyle}>
+                    <a className="dropdown-item" href="#" /*style={myStyle}*/>
                       Graph
                     </a>
                   </li>
                   <li>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#" style={myStyle}>
+                    <a className="dropdown-item" href="#" /*style={myStyle}*/>
                       AAHHH Shit! He's up again.
                     </a>
                   </li>
                 </ul>
               </li>
             </ul>
-            <div className="form-check form-switch" style={myStyle}>
+            <div className="form-check form-switch" /*style={myStyle}*/>
               <input
                 className="form-check-input"
                 type="checkbox"
                 role="switch"
                 id="flexSwitchCheckDefault"
-                onClick={toggleMode}
+                // onClick={toggleMode}
               />
               <label
                 className="form-check-label"
                 htmlFor="flexSwitchCheckDefault"
               >
-                {btnText}
+                {/* {btnText} */}
               </label>
             </div>
           </div>
