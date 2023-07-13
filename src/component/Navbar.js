5a90af9001f9368b4277b9c774b1bf23cade73e5
Ronny599 <102850680+Ronny599@users.noreply.github.com>
2023-07-13 20:50:42 +0530

    1 file modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -91,7 +91,7 @@ export default function Navigate(props) {
                 className="form-check-label"
                 htmlFor="flexSwitchCheckDefault"
               >
-                Enable Dark Mode
+                xyz
                 {/* {btnText} */}
               </label>
             </div>
